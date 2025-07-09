752b81a8d5aeade89eb81389344995f6ef5ada99
Brayan <22300043@uttt.edu.mx>
2025-07-08 20:07:43 -0600

    Actualizar conexión a API en api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 // src/api/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'https://microservicioautores.somee.com/api';
+const API_BASE_URL = 'https://elbooo.somee.com//api';
 
 export const crearAutor = async (autor) => {
   try {
